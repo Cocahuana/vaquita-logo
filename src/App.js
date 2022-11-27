e335dfd58f4c21698ac4618d84c537886f745e04
Cocahuana <ezequieldominguez11@gmail.com>
2022-11-27 19:14:11 -0300

    Feat: Title change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { ChakraProvider, Heading, Image, Flex, Button } from "@chakra-ui/react";
+import vaquitaLogo from "./vaquita.jpeg";
+import Footer from "./components/Footer";
+import { CTA } from "./components/CTA";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const colorLogo = "#5ac4c2";
+	return (
+		<ChakraProvider>
+			<div className='App'>
+				<Flex
+					fontFamily='Kitsch'
+					align='center'
+					justifyContent='center'
+					w='full'
+					h='90vh'
+					bg={colorLogo}
+					color='white'
+				>
+					<Flex
+						align='center'
+						justifyContent='center'
+						flexDirection='column'
+						w={{ base: "100%", lg: "75%" }}
+						h={{ base: "100vh", lg: "100vh" }}
+					>
+						<Heading
+							as='h1'
+							size={{ base: "md", lg: "2xl" }}
+							fontFamily='Kitsch'
+						>
+							Bienvenidos
+						</Heading>
+						<Flex align='center' justifyContent='space-around'>
+							<Flex>
+								<Image src={vaquitaLogo} />
+							</Flex>
+							<Flex>
+								<CTA />
+							</Flex>
+						</Flex>
+					</Flex>
+				</Flex>
+			</div>
+			<Footer />
+		</ChakraProvider>
+	);
 }
 
 export default App;
---
+++ src/components/CTA.jsx
@@ -0,0 +1,86 @@
+import {
+	Box,
+	Button,
+	Container,
+	Heading,
+	Stack,
+	Text,
+	useBreakpointValue,
+} from "@chakra-ui/react";
+import * as React from "react";
+
+const colorLogo = "#5ac4c2";
+export const CTA = () => (
+	<Box as='section' bg='bg-surface' fontFamily='Kitsch'>
+		<Container
+			py={{
+				base: "16",
+				md: "24",
+			}}
+		>
+			<Stack
+				spacing={{
+					base: "8",
+					md: "10",
+				}}
+			>
+				<Stack
+					spacing={{
+						base: "4",
+						md: "5",
+					}}
+					align='center'
+				>
+					<Heading
+						size={useBreakpointValue({
+							base: "sm",
+							md: "md",
+							lg: "lg",
+							xl: "2xl",
+						})}
+						fontFamily='Kitsch'
+						fontWeight='bold'
+					>
+						Te animas a probar?
+					</Heading>
+				</Stack>
+				<Stack
+					spacing='3'
+					direction={{
+						base: "column",
+						sm: "row",
+					}}
+					justify='center'
+				>
+					<Button
+						border='2px solid white'
+						variant='outline'
+						size='lg'
+						transition='ease 1s'
+						_hover={{
+							bg: "white",
+							color: colorLogo,
+						}}
+					>
+						En otro momento...
+					</Button>
+					<Button
+						as='a'
+						href='https://www.instagram.com/breathe.mendoza/'
+						target='_blank'
+						border='2px solid white'
+						variant='outline'
+						size='lg'
+						transition='ease 1s'
+						_hover={{
+							bg: "white",
+							color: colorLogo,
+						}}
+					>
+						De una
+					</Button>
+				</Stack>
+			</Stack>
+		</Container>
+	</Box>
+);
