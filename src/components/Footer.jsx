d35df1efcd0cf75eebdbde0910ca1e6d709c58fa
Rodri Soriano <rosoria010398@gmail.com>
2022-11-27 22:32:23 -0300

    removed tw and added wpp with link to chat

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,27 +6,26 @@ import {
 	Text,
 	useColorModeValue,
 	VisuallyHidden,
-} from "@chakra-ui/react";
-import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+} from '@chakra-ui/react';
+import { FaInstagram, FaTwitter, FaWhatsapp, FaYoutube } from 'react-icons/fa';
 
 const SocialButton = ({ children, label, href }) => {
 	return (
 		<chakra.button
-			bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
-			rounded={"full"}
+			bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+			rounded={'full'}
 			w={8}
 			h={8}
-			cursor={"pointer"}
-			as={"a"}
+			cursor={'pointer'}
+			as={'a'}
 			href={href}
-			display={"inline-flex"}
-			alignItems={"center"}
-			justifyContent={"center"}
-			transition={"background 0.3s ease"}
+			display={'inline-flex'}
+			alignItems={'center'}
+			justifyContent={'center'}
+			transition={'background 0.3s ease'}
 			_hover={{
-				bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
-			}}
-		>
+				bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+			}}>
 			<VisuallyHidden>{label}</VisuallyHidden>
 			{children}
 		</chakra.button>
@@ -36,30 +35,26 @@ const SocialButton = ({ children, label, href }) => {
 export default function Footer() {
 	return (
 		<Box
-			bg={useColorModeValue("gray.50", "gray.900")}
-			color={useColorModeValue("gray.700", "gray.200")}
-		>
+			bg={useColorModeValue('gray.50', 'gray.900')}
+			color={useColorModeValue('gray.700', 'gray.200')}>
 			<Container
 				as={Stack}
-				maxW={"6xl"}
+				maxW={'6xl'}
 				py={4}
-				direction={{ base: "column", md: "row" }}
+				direction={{ base: 'column', md: 'row' }}
 				spacing={4}
-				justify={{ base: "center", md: "space-between" }}
-				align={{ base: "center", md: "center" }}
-			>
+				justify={{ base: 'center', md: 'space-between' }}
+				align={{ base: 'center', md: 'center' }}>
 				<Text>© 2022 Begin Vegan. All rights reserved</Text>
-				<Stack direction={"row"} spacing={6}>
-					<SocialButton label={"Twitter"} href={"#"}>
-						<FaTwitter />
-					</SocialButton>
-					<SocialButton label={"YouTube"} href={"#"}>
-						<FaYoutube />
+				<Stack direction={'row'} spacing={6}>
+					<SocialButton
+						label={'Whatsapp'}
+						href={'https://wa.me/+5492612484447'}>
+						<FaWhatsapp />
 					</SocialButton>
 					<SocialButton
-						label={"Instagram"}
-						href={"https://www.instagram.com/breathe.mendoza/"}
-					>
+						label={'Instagram'}
+						href={'https://www.instagram.com/breathe.mendoza/'}>
 						<FaInstagram />
 					</SocialButton>
 				</Stack>
